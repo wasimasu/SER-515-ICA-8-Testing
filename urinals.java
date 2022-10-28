94562e3ffb2491fbc6954a284a846a9a5a514824
wasimasu <112654661+wasimasu@users.noreply.github.com>
2022-10-28 12:08:37 +0530

    Add files via upload
    commit - read file

--- urinals.java
+++ urinals.java
@@ -47,15 +47,57 @@ public class urinals{
         }
         return true;
     }
+    public String readFile(String filename)
+    {
+        //the path changes w.r.t where file is stored
+        String f = "out/production/ICA 8 - Testing/" + filename;
+        try {
+            File file = new File(f);
+            Scanner scr = new Scanner(file);
+            //String str = this.getOutputFileName();
+            while (scr.hasNextLine()) {
+                String line = scr.nextLine();
+                int a = this.countUrinals(line);
+               // this.writeToFile(a,str);
+                System.out.println(a);
+            }
+            return "File found and processed properly";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "File doesn't exist";
+        }
+    }
 
     public static void main(String[] args) {
         urinals obj = new urinals();
-
+        System.out.println("Do you want to import inputs from a file or by console?");
+        System.out.println("1. File");
+        System.out.println("2. Console");
         Scanner sc = new Scanner(System.in);
-        String s;
-        System.out.println("Enter String : ");
-        s = sc.next();
-        int ans = obj.countUrinals(s);
-        System.out.println(ans);
+        int choice = sc.nextInt();
+        switch(choice)
+        {
+            case 1:
+            {
+                System.out.println("Enter name of the input file : ");
+                String str = sc.next();
+                System.out.println(obj.readFile(str));
+                break;
+            }
+            case 2:
+            {
+                String s;
+                char c='N';
+                do {
+                    System.out.println("Enter String : ");
+                    s = sc.next();
+                    int ans = obj.countUrinals(s);
+                    System.out.println("Continue input (Y/N)?");
+                    c = sc.next().charAt(0);
+                }while(c=='Y');
+            }
+
+        }
+
     }
 }
--- urinalsTest.java
+++ urinalsTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class urinalsTest {
 
 
-
+   //count urinals
     @org.junit.jupiter.api.Test
     void logicTest1(){
         assertEquals(2,urinals.countUrinals("0000"));
@@ -16,4 +16,10 @@ public class urinalsTest {
         assertEquals(-1,urinals.countUrinals("aaabbb"));
     }
 
+    //read file testcases
+    @org.junit.jupiter.api.Test
+    void fileFound() {
+        urinals testcase = new urinals();
+        assertEquals("File found and processed properly", testcase.readFile("urinals.dat"));
+    }
 }
