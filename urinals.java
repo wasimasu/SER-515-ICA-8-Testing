2c2e92e86a5faba49df90f0fe42fee65e27a1ceb
wasimasu <112654661+wasimasu@users.noreply.github.com>
2022-10-28 11:40:38 +0530

    Add files via upload
    commit - good string function

--- urinals.java
+++ urinals.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.util.Scanner;
 import java.io.*;
 
+import static java.lang.Character.isDigit;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -10,7 +11,8 @@ public class urinals{
     static int countUrinals(String str)
     {   int cnt=0;
         int l = str.length();
-
+        if(!goodString(str))
+            return -1;
         int i = 0;
         while(i < l)
         {
@@ -26,6 +28,24 @@ public class urinals{
         }
         return cnt;
 
+    }
+    static Boolean goodString(String str)
+    {
+        int l = str.length();
+        for(int i=1; i<l; i++)
+        {
+            if(str.charAt(i)=='1' && str.charAt(i-1)=='1')
+            {
+                return false;
+            }
+            else if(str.charAt(i) > '1'){
+                return false;
+            }
+            else if(!isDigit(str.charAt(i))){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
--- urinalsTest.java
+++ urinalsTest.java
@@ -10,4 +10,10 @@ public class urinalsTest {
         assertEquals(2,urinals.countUrinals("0000"));
     }
 
+    //testcase for goodstring
+    @org.junit.jupiter.api.Test
+    void invalidInput(){
+        assertEquals(-1,urinals.countUrinals("aaabbb"));
+    }
+
 }
